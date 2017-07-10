7dc5d5c8a6a68ae6b0067999e38595a99613bf48
Chris Foster <chris42f@gmail.com>
2017-07-10 20:52:32 +1000

    Initial code, works for expanding matvec with triangular matrices

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # SimpleSymbolic
+
+A tiny package for symbolic manipulation of julia expressions, aimed at code
+generation tasks.  Example:
+
+```julia
+julia> using SimpleSymbolic
+
+julia> m = [S(:(m[$i,$j])) for i=1:3, j=1:3]
+3×3 Array{SimpleSymbolic.S,2}:
+ S(m[1, 1])  S(m[1, 2])  S(m[1, 3])
+ S(m[2, 1])  S(m[2, 2])  S(m[2, 3])
+ S(m[3, 1])  S(m[3, 2])  S(m[3, 3])
+
+julia> v = [S(:(v[$i])) for i=1:3]
+3-element Array{SimpleSymbolic.S,1}:
+ S(v[1])
+ S(v[2])
+ S(v[3])
+
+julia> m*v
+3-element Array{SimpleSymbolic.S,1}:
+ S((m[1, 1] * v[1] + m[1, 2] * v[2]) + m[1, 3] * v[3])
+ S((m[2, 1] * v[1] + m[2, 2] * v[2]) + m[2, 3] * v[3])
+ S((m[3, 1] * v[1] + m[3, 2] * v[2]) + m[3, 3] * v[3])
+
+julia> LowerTriangular(m)*v
+3-element Array{SimpleSymbolic.S,1}:
+                                     S(m[1, 1] * v[1])
+                    S(m[2, 2] * v[2] + m[2, 1] * v[1])
+ S((m[3, 3] * v[3] + m[3, 1] * v[1]) + m[3, 2] * v[2])
+```
--- src/SimpleSymbolic.jl
+++ src/SimpleSymbolic.jl
@@ -1,5 +1,67 @@
 module SimpleSymbolic
 
-# package code goes here
+export S
+
+"""
+    S(x::Symbol)
+    S(x::Expr)
+    S(x::Number)
+
+Create a symbolic wrapper `S <: Number` for the object `x`.
+"""
+struct S <: Number
+    x
+end
+
+Base.show(io::IO, s::S) = print(io, "S($(s.x))")
+
+# Promotion & conversion wraps types in `S`
+Base.convert(::Type{S}, x::S) = x
+Base.convert(::Type{S}, x) = S(x)
+Base.promote_rule(::Type{S}, ::Type{<:Any}) = S
+
+# Additive & multiplicative identity
+struct Zero ; end
+struct One  ; end
+
+Base.zero(::Type{S}) = S(Zero())
+Base.one(::Type{S}) = S(One())
+Base.zero(::S) = zero(S)
+Base.one(::S) = one(S)
+
+# Arithmetic on `S` is implemented by unwrapping and rewrapping so that we can
+# use the standard promotion machinery but also do some basic simplifications
+# on the fly.
+_funcname(f) = typeof(f).name.mt.name # See Base show(::IO, ::Function)
+_op(f, es...) = Expr(:call, _funcname(f), es...)
+
+unwrap() = ()
+unwrap(s::S, ss::S...) = (s.x, unwrap(ss...)...)
+
+Base.:+(s1::S, ss::S...) = S(_op(+, unwrap(s1, ss...)...))
+Base.:*(s1::S, ss::S...) = S(_op(*, unwrap(s1, ss...)...))
+Base.:-(s1::S, ss::S...) = S(_op(-, unwrap(s1, ss...)...))
+Base.:/(s1::S, ss::S...) = S(_op(/, unwrap(s1, ss...)...))
+
+for func in [:sqrt, :exp, :log, :sin, :cos, :conj]
+    @eval Base.$func(s::S) = S(_op($func, s.x))
+end
+
+# Basic simplification
+# Simplification for numbers
+_op(f, xs::Number...) = f(xs...)
+
+# Simplification for standard left-fold reductions
+iscall(x::Expr, name) = x.head == :call && x.args[1] == name
+
+_op(f::typeof(+), x::Zero, x2) = x2
+_op(f::typeof(+), x::Zero, xs...) = _op(f, xs...)
+
+_op(f::typeof(*), x::Zero, xs...) = Zero()
+_op(f::typeof(*), x::One, x2) = x2
+_op(f::typeof(*), x::One, xs...) = _op(f, xs...)
+
+_op(f::typeof(conj), x::Expr) = iscall(x, :conj) ? x.args[2] : Expr(:call, _funcname(f), x)
 
 end # module
+
